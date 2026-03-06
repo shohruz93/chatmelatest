238ee6bb230fed79855fe8fbf313e8f34ed68c44
shohruz93 <shukurovshohruz@gmail.com>
2026-03-06 09:42:07 +0500

    feat: Add Google authentication service using a local HTTP listener for OAuth flow.

--- windows/ChatmeWindows/obj/Debug/net10.0-windows10.0.19041.0/ChatmeWindows.AssemblyInfo.cs
+++ windows/ChatmeWindows/obj/Debug/net10.0-windows10.0.19041.0/ChatmeWindows.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatmeWindows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127971018706aedead4f7ec33e3bc3e790d74ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb22f5c24ba863532f89e43da40681eee7b596f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatmeWindows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatmeWindows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
